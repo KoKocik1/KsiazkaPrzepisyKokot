224b13e9f51634ad297d346ac5ec32bd2199a922
Krzysztof Kokot <krzykok829@student.polsl.pl>
2022-03-21 08:42:03 +0100

    Dodaj pliki projektów.

---
+++ KsiazkaPrzepisyKokot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KsiazkaPrzepisyKokot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
